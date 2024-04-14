b4cf798b4a708c9254c8183167fe56c3d4bed31d
WKHAllen <wkhallen@gmail.com>
2024-04-14 16:20:32 -0400

    Implement rectangle collision

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const wallDepth = 16;
+
 class Player {
   constructor(x, y, d, color) {
     this.x = x;
@@ -7,22 +9,67 @@ class Player {
     this.color = color;
     this.vel = {
       x: 1,
-      y: 1
+      y: 1,
     };
   }
 }
 
-class Line {
-  constructor(x1, y1, x2, y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
+}
+
+class Line {
+  constructor(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+
   length() {
     return Math.sqrt(
-      Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2)
+      Math.pow(this.p2.x - this.p1.x, 2) + Math.pow(this.p2.y - this.p1.y, 2)
     );
+  }
+}
+
+class Rectangle {
+  constructor(l1, l2, l3, l4) {
+    this.l1 = l1;
+    this.l2 = l2;
+    this.l3 = l3;
+    this.l4 = l4;
+  }
+
+  pointInside(point) {
+    const d1 =
+      (this.l1.p2.x - this.l1.p1.x) * (point.y - this.l1.p1.y) -
+      (this.l1.p2.y - this.l1.p1.y) * (point.x - this.l1.p1.x);
+    const d2 =
+      (this.l2.p2.x - this.l2.p1.x) * (point.y - this.l2.p1.y) -
+      (this.l2.p2.y - this.l2.p1.y) * (point.x - this.l2.p1.x);
+    const d3 =
+      (this.l3.p2.x - this.l3.p1.x) * (point.y - this.l3.p1.y) -
+      (this.l3.p2.y - this.l3.p1.y) * (point.x - this.l3.p1.x);
+    const d4 =
+      (this.l4.p2.x - this.l4.p1.x) * (point.y - this.l4.p1.y) -
+      (this.l4.p2.y - this.l4.p1.y) * (point.x - this.l4.p1.x);
+
+    return d1 <= 0 && d2 <= 0 && d3 <= 0 && d4 <= 0;
+  }
+
+  draw() {
+    push();
+
+    stroke(255);
+    strokeWeight(2);
+    line(this.l1.p1.x, this.l1.p1.y, this.l1.p2.x, this.l1.p2.y);
+    line(this.l2.p1.x, this.l2.p1.y, this.l2.p2.x, this.l2.p2.y);
+    line(this.l3.p1.x, this.l3.p1.y, this.l3.p2.x, this.l3.p2.y);
+    line(this.l4.p1.x, this.l4.p1.y, this.l4.p2.x, this.l4.p2.y);
+
+    pop();
   }
 }
 
@@ -30,6 +77,7 @@ class DrawingLine {
   constructor() {
     this.drawing = false;
     this.line = undefined;
+    this.rect = undefined;
     this.startX = undefined;
     this.startY = undefined;
   }
@@ -37,13 +85,38 @@ class DrawingLine {
   drawStart(x, y) {
     this.drawing = true;
     this.line = undefined;
+    this.rect = undefined;
     this.startX = x;
     this.startY = y;
   }
 
   drawEnd(x, y) {
+    const angle = Math.atan2(y - this.startY, x - this.startX) + Math.PI / 2;
+    const p1 = new Point(
+      this.startX - (wallDepth / 2) * Math.cos(angle),
+      this.startY - (wallDepth / 2) * Math.sin(angle)
+    );
+    const p2 = new Point(
+      this.startX - (wallDepth / 2) * Math.cos(angle + Math.PI),
+      this.startY - (wallDepth / 2) * Math.sin(angle + Math.PI)
+    );
+    const p3 = new Point(
+      x - (wallDepth / 2) * Math.cos(angle + Math.PI),
+      y - (wallDepth / 2) * Math.sin(angle + Math.PI)
+    );
+    const p4 = new Point(
+      x - (wallDepth / 2) * Math.cos(angle),
+      y - (wallDepth / 2) * Math.sin(angle)
+    );
+
     this.drawing = false;
-    this.line = new Line(this.startX, this.startY, x, y);
+    this.line = new Line(new Point(this.startX, this.startY), new Point(x, y));
+    this.rect = new Rectangle(
+      new Line(p1, p2),
+      new Line(p2, p3),
+      new Line(p3, p4),
+      new Line(p4, p1)
+    );
     this.startX = undefined;
     this.startY = undefined;
   }
@@ -51,32 +124,50 @@ class DrawingLine {
   clearDrawing() {
     this.drawing = false;
     this.line = undefined;
+    this.rect = undefined;
     this.startX = undefined;
     this.startY = undefined;
   }
 }
 
 const drawing = new DrawingLine();
-
+const circle1 = new Player(50, 50, 16, "red");
 
 function setup() {
   createCanvas(800, 600);
   stroke(255);
   frameRate(30);
-  circle1 = new Player(50, 50, 30, 'red');
 }
 
 function draw() {
   stroke(255);
   background(0);
+
   if (drawing.drawing) {
+    stroke(255, 0, 0);
+    strokeWeight(2);
     line(drawing.startX, drawing.startY, mouseX, mouseY);
-  } else if (drawing.line !== undefined) {
-    line(drawing.line.x1, drawing.line.y1, drawing.line.x2, drawing.line.y2);
+  } else if (drawing.rect !== undefined) {
+    drawing.rect.draw();
+    push();
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    line(
+      drawing.line.p1.x,
+      drawing.line.p1.y,
+      drawing.line.p2.x,
+      drawing.line.p2.y
+    );
+    pop();
   }
-  if (checkPaddleCollision()) {
-    stroke('magenta');
+
+  strokeWeight(0);
+  if (checkWallCollision()) {
+    fill(63, 0, 255);
+  } else {
+    fill(255);
   }
+
   updatePlayerCoor();
 }
 
@@ -86,60 +177,10 @@ function updatePlayerCoor() {
   circle(circle1.x, circle1.y, circle1.d);
 }
 
-function checkPaddleCollision() {
-  if (drawing.line == undefined) {
-    return false;
-  }
-
-  let x1 = Math.min(drawing.line.x1, drawing.line.x2);
-  let x2 = Math.max(drawing.line.x1, drawing.line.x2);
-  let y1 = Math.min(drawing.line.y1, drawing.line.y2);
-  let y2 = Math.max(drawing.line.y1, drawing.line.y2);
-
-  //checking whether the line is vertical 
-  if (x1 === x2) {
-
-    //are interecepts within the radius of the circle?
-    let yInt1 = circle1.y - Math.sqrt(circle1.r * circle1.r - (x1 - circle1.x) * (x1 - circle1.x));
-    let y2 = circle1.y + Math.sqrt(circle1.r * circle1.r - (x1 - circle1.x) * (x1 - circle1.x));
-    if ((y1 >= y1 && y1 <= y2) || (y2 >= y1 && y2 <= y2)) {
-      //yes
-      return true;
-    }
-  }
-  else {
-    //Line slope for perp distance calculation
-    let m = (y2 - y1) / (x2 - x1);
-    //Line y for perp distance calculation
-    let b = y1 - m * x1;
-
-    //perpendicular distance from ball to line
-    let distance = Math.abs(circle1.y - m * circle1.x - b) / Math.sqrt(m * m + 1);
-    //checks if the ball is 'inside' the line on the y axis
-    if (distance <= circle1.r) {
-      // if the line is vertical
-      //getting 2 intercept points (x-bound)
-      let y1 = m * circle1.x + b;
-      let x1 = (y1 - b) / m;
-      let y2 = m * (circle1.x + circle1.r) + b;
-      let x2 = (y2 - b) / m;
-
-
-      
-      //special case: horizontal line. checks y interecepts instead of x since y would be touching the line sooner
-      if (m === 0) {
-        if ((y1 >= y1 && y1 <= y2) || (y2 >= y1 && y2 <= y2)) {
-          return true;
-        }
-      }
-      else {
-        if ((x1 >= x1 && x1 <= x2) || (x2 >= x1 && x2 <= x2)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
+function checkWallCollision() {
+  return drawing.rect !== undefined
+    ? drawing.rect.pointInside(new Point(circle1.x, circle1.y))
+    : false;
 }
 
 function mousePressed() {
